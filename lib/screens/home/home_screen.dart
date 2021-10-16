a79196eeebbc0006e35453ad58ab5cbcd96c4058
Leobelle Joseph <leobellejoseph@outlook.com>
2021-10-16 20:34:53 +0800

    UI Design

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -18,40 +18,37 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: [
-            SafeArea(
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.blue.withOpacity(0.8),
-                      Colors.blue.withOpacity(0.4),
-                    ],
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: const [
-                    NowCastWidget(
-                        title: '2-hour Nowcast',
-                        subtitle: '5:00 pm to 7:00 pm'),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: 5, right: 5, top: 10, bottom: 10),
-                      child: Divider(height: 1, color: Colors.white60),
-                    ),
-                    TodayCastWidget(),
-                    // ObservationWidget(),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.blue.withOpacity(0.8),
+                    Colors.blue.withOpacity(0.4),
                   ],
                 ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  NowCastWidget(
+                      title: '2-hour Nowcast', subtitle: '5:00 pm to 7:00 pm'),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+                    child: Divider(height: 1, color: Colors.white60),
+                  ),
+                  TodayCastWidget(),
+                  // ObservationWidget(),
+                ],
               ),
             ),
             Positioned(
               left: 10,
-              top: 30,
+              top: 50,
               child: ImageButton(asset: 'images/mss.png', onPress: () {}),
             ),
           ],
