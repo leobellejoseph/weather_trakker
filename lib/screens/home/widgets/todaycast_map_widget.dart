9c7756f8bd16d23b629a9bec3f172f1256738615
Leobelle Joseph <leobellejoseph@outlook.com>
2021-10-20 08:10:11 +0800

    hight adjustments and corrections

--- lib/screens/home/widgets/todaycast_map_widget.dart
+++ lib/screens/home/widgets/todaycast_map_widget.dart
@@ -10,50 +10,45 @@ class TodaycastMapItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 2, right: 2, bottom: 10.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          HeaderWidget(
-              title: period.time.description,
-              subtitle: '${period.time.start} - ${period.time.end}'),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(8),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 30, right: 70, bottom: 75, top: 10),
-                child: Stack(
-                  children: [
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child:
-                            kWeatherStatus[period.region.west] ?? Container()),
-                    Align(
-                        alignment: Alignment.centerRight,
-                        child:
-                            kWeatherStatus[period.region.east] ?? Container()),
-                    Align(
-                        alignment: Alignment.center,
-                        child: kWeatherStatus[period.region.central] ??
-                            Container()),
-                    Align(
-                        alignment: Alignment.topCenter,
-                        child:
-                            kWeatherStatus[period.region.north] ?? Container()),
-                    Align(
-                        alignment: Alignment.bottomCenter,
-                        child:
-                            kWeatherStatus[period.region.south] ?? Container()),
-                  ],
-                ),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        HeaderWidget(
+            title: period.time.description,
+            subtitle: '${period.time.start} - ${period.time.end}'),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 30, right: 70, bottom: 80, top: 5),
+              child: Stack(
+                children: [
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: kWeatherStatus[period.region.west] ?? Container()),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: kWeatherStatus[period.region.east] ?? Container()),
+                  Align(
+                      alignment: Alignment.center,
+                      child:
+                          kWeatherStatus[period.region.central] ?? Container()),
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child:
+                          kWeatherStatus[period.region.north] ?? Container()),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child:
+                          kWeatherStatus[period.region.south] ?? Container()),
+                ],
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
