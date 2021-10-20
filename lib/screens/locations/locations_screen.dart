db69ba61b7218bdc955dd7479c65dc7b09ddba4d
Leobelle Joseph <leobellejoseph@outlook.com>
2021-10-20 21:45:46 +0800

    bug fixes

--- lib/screens/locations/locations_screen.dart
+++ lib/screens/locations/locations_screen.dart
@@ -7,7 +7,7 @@ import 'package:weather_trakker/cubit/cubit.dart';
 import 'package:weather_trakker/screens/locations/widgets/location_tile_widget.dart';
 import 'package:weather_trakker/widgets/widgets.dart';
 
-class LocationsScreen extends StatelessWidget {
+class LocationsScreen extends HookWidget {
   static const id = 'locations';
 
   static Route route() => MaterialPageRoute(
@@ -18,6 +18,8 @@ class LocationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = useTextEditingController();
+    final focus = useFocusNode();
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: InkWellButton(
@@ -44,34 +46,74 @@ class LocationsScreen extends StatelessWidget {
               return SafeArea(
                 child: Column(
                   children: [
-                    const _SearchWidget(key: ValueKey('searchFavorites')),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(
-                            left: 15, right: 15, bottom: 70),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(14),
-                          border: Border.all(color: Colors.white, width: 0.5),
-                        ),
-                        child: BlocBuilder<FavoritesCubit, FavoritesState>(
-                          builder: (_, favorites) {
-                            return ListView.separated(
-                                itemBuilder: (context, index) {
-                                  final item =
-                                      state.data.items[0].forecasts[index];
-                                  bool isFavorite = favorites.data
-                                      .where((i) => i.area == item.area)
-                                      .isNotEmpty;
-                                  return LocationTile(
-                                      item: item, favorite: isFavorite);
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      margin:
+                          const EdgeInsets.only(left: 15, right: 15, bottom: 2),
+                      width: double.infinity,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        border: Border.all(color: Colors.white, width: 0.5),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Bounceable(
+                              onTap: () {
+                                //TODO:fetch filtered data
+                              },
+                              child: const Icon(Icons.search)),
+                          Expanded(
+                            child: EditableText(
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                onSubmitted: (value) {
+                                  //TODO:fetch filtered data
                                 },
-                                itemCount: state.data.items[0].forecasts.length,
-                                separatorBuilder:
-                                    (BuildContext context, int index) =>
-                                        const Divider(
-                                            thickness: 0.1,
-                                            color: Colors.white));
-                          },
+                                focusNode: focus,
+                                backgroundCursorColor: Colors.blue,
+                                controller: controller,
+                                cursorColor: Colors.black,
+                                style: const TextStyle(fontSize: 18)),
+                          ),
+                          Bounceable(
+                              onTap: () => controller.clear(),
+                              child: const Icon(Icons.clear)),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () => focus.unfocus(),
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 15, right: 15, bottom: 70),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            border: Border.all(color: Colors.white, width: 0.5),
+                          ),
+                          child: BlocBuilder<FavoritesCubit, FavoritesState>(
+                            builder: (_, favorites) {
+                              return ListView.separated(
+                                  itemBuilder: (context, index) {
+                                    final item =
+                                        state.data.items[0].forecasts[index];
+                                    bool isFavorite = favorites.data
+                                        .where((i) => i.area == item.area)
+                                        .isNotEmpty;
+                                    return LocationTile(
+                                        item: item, favorite: isFavorite);
+                                  },
+                                  itemCount:
+                                      state.data.items[0].forecasts.length,
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          const Divider(
+                                              thickness: 0.1,
+                                              color: Colors.white));
+                            },
+                          ),
                         ),
                       ),
                     ),
@@ -86,47 +128,47 @@ class LocationsScreen extends StatelessWidget {
   }
 }
 
-class _SearchWidget extends HookWidget {
-  const _SearchWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final focus = useFocusNode()..requestFocus();
-    return Container(
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.only(left: 15, right: 15, bottom: 2),
-      width: double.infinity,
-      height: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        border: Border.all(color: Colors.white, width: 0.5),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Bounceable(
-              onTap: () {
-                //TODO:fetch filtered data
-              },
-              child: const Icon(Icons.search)),
-          Expanded(
-            child: EditableText(
-                enableSuggestions: false,
-                autocorrect: false,
-                onSubmitted: (value) {
-                  //TODO:fetch filtered data
-                },
-                focusNode: focus,
-                backgroundCursorColor: Colors.blue,
-                controller: controller,
-                cursorColor: Colors.black,
-                style: const TextStyle(fontSize: 18)),
-          ),
-          Bounceable(
-              onTap: () => controller.clear(), child: const Icon(Icons.clear)),
-        ],
-      ),
-    );
-  }
-}
+// class _SearchWidget extends HookWidget {
+//   const _SearchWidget({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = useTextEditingController();
+//     final focus = useFocusNode()..requestFocus();
+//     return Container(
+//       padding: const EdgeInsets.all(8),
+//       margin: const EdgeInsets.only(left: 15, right: 15, bottom: 2),
+//       width: double.infinity,
+//       height: 40,
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(14),
+//         border: Border.all(color: Colors.white, width: 0.5),
+//       ),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           Bounceable(
+//               onTap: () {
+//                 //TODO:fetch filtered data
+//               },
+//               child: const Icon(Icons.search)),
+//           Expanded(
+//             child: EditableText(
+//                 enableSuggestions: false,
+//                 autocorrect: false,
+//                 onSubmitted: (value) {
+//                   //TODO:fetch filtered data
+//                 },
+//                 focusNode: focus,
+//                 backgroundCursorColor: Colors.blue,
+//                 controller: controller,
+//                 cursorColor: Colors.black,
+//                 style: const TextStyle(fontSize: 18)),
+//           ),
+//           Bounceable(
+//               onTap: () => controller.clear(), child: const Icon(Icons.clear)),
+//         ],
+//       ),
+//     );
+//   }
+// }
