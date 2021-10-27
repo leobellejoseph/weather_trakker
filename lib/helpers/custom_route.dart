6aa98d4aaefb69574850d4ed5dc0efab4a1ee7bd
Leobelle Joseph <leobellejoseph@outlook.com>
2021-10-27 21:51:22 +0800

    cleanup

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:weather_trakker/screens/screens.dart';
-import 'package:weather_trakker/screens/splashscreen/location_permission_screen.dart';
-import 'package:weather_trakker/screens/splashscreen/location_settings_screen.dart';
 
 class CustomRoute {
   static Route onGenerateRoute(RouteSettings settings) {
@@ -16,10 +14,6 @@ class CustomRoute {
         return LocationsScreen.route();
       case ForecastScreen.id:
         return ForecastScreen.route();
-      case LocationSettings.id:
-        return LocationSettings.route();
-      case LocationPermissionScreen.id:
-        return LocationPermissionScreen.route();
       default:
         return MaterialPageRoute(
           builder: (context) => const Scaffold(
